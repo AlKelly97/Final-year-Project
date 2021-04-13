4783c3eeeb21196a0f03e9db780e0223b7d1ed81
AlKelly97 <x00138464@gmail.com>
2021-04-13 16:25:58 +0100

    BackUp repo from deletion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+//Importing the necessary dependancies for the project
 import './App.css';
+import NewsList from './components/NewsList';
+import $ from "jquery"
+import React from 'react'
 
+//The App component handles all of the rendering in the browser.
 function App() {
+
+  //Using Jquery to call and render the API data fro covidAPI. 
+  //Uses a GET request to return Confirmed cases, Recovered and Deaths
+  $.getJSON("https://api.covid19api.com/total/country/IE", function (api) {
+    console.log(api)
+    var Confirmed = api[api.length - 1].Confirmed
+    var Deaths = api[api.length - 1].Deaths;
+    var Recovered = api[api.length - 1].Recovered;
+
+    $(".Confirmed").html('<strong>Confirmed:</strong>' + Confirmed);
+    $(".Deaths").html('<strong>Deaths:</strong>' + Deaths);
+    $(".Recovered").html('<strong>Recovered:</strong>' + Recovered);
+  });
+
+  //Simple HTML content below:
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <nav>
+        <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/news">News</a></li>
+        <li><a href="tips.html">Lockdown Tips</a></li>
+        <li><a href="/Posts">Discussions</a></li>
+    </ul>
+        </nav>
+
+        </header>
+        <body>
+          <p>
+            CoronaVirus
+          </p>
+
+
+<div>
+  <h3>Ireland Case stats</h3>
+        <p class="Confirmed"></p>
+        <p class="Deaths"></p>
+        <p class="Recovered"></p>
+</div>
+
+          {/*Calls the NewsList Component and displays it all from the below tag.*/}
+          <NewsList/>
+          
+          </body>
+       
+     
+      
     </div>
   );
 }
+
+
+
+
+
 
 export default App;
